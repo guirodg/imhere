811657e86e2019cdce0dd5851a9f5fc04c43e19d
gui <guilherme.rddev@gmail.com>
2024-05-09 17:32:51 -0300

    Atualização Generica

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,28 @@
-import { Text, View } from "react-native";
+import { Text, TextInput, View, TouchableOpacity } from "react-native";
 import { styles } from "./styles";
 
-export default function Home() {
+export function Home() {
+  function handleParticipantAdd() {
+    console.log("Voce clicou aqui");
+  }
+
   return (
-    <View className="bg-black flex-1 p-6">
-      <Text className="text-white text-2xl font-normal mr-12">
-        Nome do evento
-      </Text>
-      <Text className="text-slate-700 text-base">
-        Sexta, 4 de Novembro de 2024.
-      </Text>
+    <View className={styles.container}>
+      <Text className={styles.title}>Nome do evento</Text>
+      <Text className={styles.description}>Sexta, 4 de Novembro de 2024.</Text>
+
+      <TextInput
+        className={styles.input}
+        placeholder="Nome do participante"
+        placeholderTextColor={"#6B6B6B"}
+      />
+
+      <TouchableOpacity
+        className={styles.button}
+        onPress={handleParticipantAdd}
+      >
+        <Text className={styles.buttonText}>+</Text>
+      </TouchableOpacity>
     </View>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,3 +1,8 @@
 export const styles = {
   container: "bg-black flex-1 p-6",
+  title: "text-white text-2xl font-normal mr-12",
+  description: "text-slate-700 text-base",
+  input: "bg-slate-800 h-14 rounded-md text-gray-50 p-5 text-base",
+  button: "items-center justify-center w-14 h-14 rounded-md bg-green-400",
+  buttonText: "text-gray-50 text-2xl",
 };
